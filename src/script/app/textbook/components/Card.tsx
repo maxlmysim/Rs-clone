fa3335f3eb94352d2498806f85b87b5ea7843572
Naysay3r <ilia.mahnach@gmail.com>
2022-08-21 23:47:42 +0300

    feat: realize non-styled template of card

--- src/script/app/textbook/components/Card.tsx
+++ src/script/app/textbook/components/Card.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Word } from '../../../interface/server';
+import { config } from '../config';
 
-export function Card(): React.ReactElement {
+interface CardProps {
+  word: Word,
+}
+
+export function Card({ word }: CardProps): React.ReactElement {
   return (
-    <div className="textbook-card">Card</div>
+    <div className="textbook-card">
+      <img src={`${config.serverUrl}${word.image}`} alt="cardcover" />
+      <div className="card-content">
+        <h5 className="card-content-header">{`${word.word} - ${word.transcription}`}</h5>
+        <h6 className="card-content-translate">{word.wordTranslate}</h6>
+        <h6 className="card-content-meaning">{word.textMeaning}</h6>
+        <h6 className="card-content-meaning-translate">{word.textMeaningTranslate}</h6>
+        <h6 className="card-content-example">{word.textExample}</h6>
+        <h6 className="card-content-example-translate">{word.textExampleTranslate}</h6>
+      </div>
+    </div>
   );
 }
--- src/script/app/textbook/index.tsx
+++ src/script/app/textbook/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createTag } from '../../helper/helper';
 import { Card } from './components/Card';
+import { words } from './data/words';
 
 const root = createTag('div', 'textbook', '');
 document.body.appendChild(root);
@@ -10,8 +11,7 @@ const rootContainer = createRoot(root as Element);
 function Textbook(): React.ReactElement {
   return (
     <div className="textbook-wrapper">
-      <Card />
-      <Card />
+      <Card word={words[0]} />
     </div>
   );
 }
