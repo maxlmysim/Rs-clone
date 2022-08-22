e5a961303e491d0204e605a8aaf60f9a886f1055
Naysay3r <ilia.mahnach@gmail.com>
2022-08-22 22:45:35 +0300

    fix: some text should use innerHTML

--- src/script/view/pages/textbook/components/Card.tsx
+++ src/script/view/pages/textbook/components/Card.tsx
@@ -13,9 +13,9 @@ export function Card({ word, port }: CardProps): React.ReactElement {
       <div className="card-content">
         <h5 className="card-content-header">{`${word.word} - ${word.transcription}`}</h5>
         <h6 className="card-content-translate">{word.wordTranslate}</h6>
-        <h6 className="card-content-meaning">{word.textMeaning}</h6>
+        <h6 className="card-content-meaning" dangerouslySetInnerHTML={{ __html: word.textMeaning }} />
         <h6 className="card-content-meaning-translate">{word.textMeaningTranslate}</h6>
-        <h6 className="card-content-example">{word.textExample}</h6>
+        <h6 className="card-content-example" dangerouslySetInnerHTML={{ __html: word.textExample }} />
         <h6 className="card-content-example-translate">{word.textExampleTranslate}</h6>
       </div>
     </div>
