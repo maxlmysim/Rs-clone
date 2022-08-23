5463d25f3d597c13ed0fb67e743d850821818182
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-08-23 13:06:22 +0300

    fix: fix mistakes load page

--- src/script/app/app.ts
+++ src/script/app/app.ts
@@ -2,38 +2,45 @@ import { ViewApp } from '../view/viewApp';
 import { ControllerApp } from '../controller/controller';
 import { AuthorizationView } from '../authorization/authorizationView';
 import { IdPages } from '../interface/typeApp';
+import { MainPage } from '../view/pages/mainPage';
 
 export class App {
   private view: ViewApp;
 
   private controller: ControllerApp;
 
+  private mainPage : MainPage;
+
   public constructor() {
     this.view = new ViewApp();
     this.controller = new ControllerApp();
+    this.mainPage = new MainPage();
   }
 
   public async start():Promise<void> {
-    this.startPageUseHash();
+    await window.addEventListener('load', this.createMainPage.bind(this));
+    await window.addEventListener('hashchange', this.startPageUseHash.bind(this));
+  }
 
-    window.addEventListener('hashchange', this.startPageUseHash);
+  private createMainPage(): void {
+    this.controller.startPage(this.view.renderPage);
+    this.startPageUseHash();
   }
 
   private startPageUseHash():void {
     const newHash = window.location.hash.slice(1);
     switch (newHash) {
       case IdPages.login: {
-        console.log(this);
         const auth = new AuthorizationView();
         this.controller.openPage(auth.init());
         break;
       }
       case IdPages.main: {
+        this.controller.openPage(this.mainPage.create());
         break;
       }
       default: {
-        console.log(this);
-        this.controller.startPage(this.view.renderPage);
+        this.controller.openPage(this.mainPage.create());
       }
     }
   }
